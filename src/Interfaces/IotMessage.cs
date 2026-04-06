0f785e95dceb7635d2091c39d9cbb3048c29c041
Florian Bader <me@florian-bader.de>
2026-04-06 17:57:00 +0200

    Adds influx demo

--- src/Interfaces/IotMessage.cs
+++ src/Interfaces/IotMessage.cs
@@ -1,4 +1,9 @@
 ﻿namespace Interfaces
 {
-    public record IotMessage<T> ( T Message, DateTimeOffset Timestamp, string Type);
+    public record IotMessage<T>(T Message, DateTimeOffset Timestamp, string Type);
+
+    public record ProducedMessage(DateTimeOffset Timestamp, string ProductCode, int Quantity)
+    {
+        public string Name => "Produced";
+    }
 }
--- src/MqttSenders/MqttDeviceSimulator.cs
+++ src/MqttSenders/MqttDeviceSimulator.cs
@@ -8,6 +8,14 @@ namespace MqttSenders
 {
     public class MqttDeviceSimulator(ILogger<MqttDeviceSimulator> logger) : BackgroundService
     {
+        private static readonly string[] _productCodes =
+        {
+            "Screws",
+            "Nails",
+            "Bolts",
+            "Hammers",
+            "Screwdrivers",
+        };
         private readonly ILogger<MqttDeviceSimulator> _logger = logger;
         private readonly Random _random = new();
         private readonly MqttClientFactory _mqttFactory = new();
@@ -15,22 +23,32 @@ namespace MqttSenders
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             using var mqttClient = _mqttFactory.CreateMqttClient();
-            var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost", 1883).Build();
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer("localhost", 1883)
+                .Build();
             await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                var temperature = _random.NextInt64(-10, 40);
-                var iotMessage = new IotMessage<long>(temperature, DateTimeOffset.Now, "telemetry");
+                var quantity = _random.Next(1, 10);
+                var productCode = _productCodes[_random.Next(_productCodes.Length)];
+                var iotMessage = new ProducedMessage(DateTimeOffset.Now, productCode, quantity);
                 var payload = JsonSerializer.Serialize(iotMessage);
 
                 var applicationMessage = new MqttApplicationMessageBuilder()
-                    .WithTopic("temperature/living_room")
+                    .WithTopic("telemetry")
                     .WithPayload(payload)
                     .Build();
 
-                _logger.LogInformation("Publishing Message at {Timestamp} with Temperature: {Temperature}", DateTimeOffset.Now, temperature);
+                _logger.LogInformation(
+                    "Publishing Message at {Timestamp} with ProductCode: {ProductCode} and Quantity: {Quantity}",
+                    DateTimeOffset.Now,
+                    productCode,
+                    quantity
+                );
+
                 await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
                 await Task.Delay(1000, stoppingToken);
             }
 
@@ -38,7 +56,8 @@ namespace MqttSenders
                 new MqttClientDisconnectOptionsBuilder()
                     .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
                     .Build(),
-                stoppingToken);
+                stoppingToken
+            );
         }
     }
 }
