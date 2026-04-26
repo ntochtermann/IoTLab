d92a0e7fc84ce6690394078679753c350dfb2f38
Nick <76399358+ntochtermann@users.noreply.github.com>
2026-04-26 18:44:19 +0000

    aufgabe2b

--- src/StreamProcessing/AggregatorAverage.cs
+++ src/StreamProcessing/AggregatorAverage.cs
@@ -9,15 +9,31 @@ namespace StreamProcessing
     {
         private readonly ILogger<AggregatorAverage> logger;
 
+        private Queue<IotMessage<double>> messageQueue;
+
+        private const int count = 10;
+
         public AggregatorAverage(ILogger<AggregatorAverage> logger)
         {
             this.logger = logger;
+            this.messageQueue = new Queue<IotMessage<double>>();
         }
 
         public IotMessage<double>? HandleMessage(IotMessage<double> message)
         {
             // TODO: Implement the logic to calculate the average based on the instructions in Übung2.md.
-            throw new NotImplementedException();
+            messageQueue.Enqueue(message);
+            logger.LogInformation("QueueLength: {length}", messageQueue.Count);
+
+            if (messageQueue.Count == count)
+            {
+                double avg = messageQueue.Average(m => m.Message);
+                messageQueue.Clear();
+
+                return new IotMessage<double>(avg, DateTimeOffset.Now, "aggregate");
+            }
+
+            return null;
         }
     }
 }
